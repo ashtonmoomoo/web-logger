4b091def52b9ac3cbc3707b2bea395c5f37d274e
Ashton Moore <ashton.chris.moore@gmail.com>
2022-10-09 21:32:38 +1300

    Remove app's legs

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: deno run --allow-env --allow-write --allow-net --allow-read mod.ts
--- mod.ts
+++
@@ -1,5 +0,0 @@
-import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
-import { PORT } from "./config.ts";
-import { handler } from "./handlers/handler.ts";
-
-await serve(handler, { port: PORT });
